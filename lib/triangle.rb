d124aa8bbd8155227514ffc89a99a8e84c3bbde5
Bala Luguja <bluguja@gmail.com>
2020-05-04 02:40:37 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,40 +1,27 @@
 class Triangle
-  attr_accessor :x, :y, :z
-  def initialize(x, y, z)
-    @x = x
-    @y = y
-    @z = z
+  attr_accessor :side_one, :side_two, :side_three
+  def initialize(side_one, side_two, side_three)
+    @s_one = side_one
+    @s_two = side_two
+    @s_three = side_three
   end
-
   def kind
-    if kosher_triangle == true && kosher_triangle2 == true 
-      puts true 
-    else 
-      raise TriangleError 
-    end 
-    
-    if x == y && y == z && x == z
-      :equilateral
-    elsif x != y && y != z && z != x
-      :scalene 
-    else 
-      :isosceles
-    end 
+    if (@s_one * @s_two * @s_three) == 0 || (@s_one + @s_two) <= @s_three || (@s_two + @s_three) <= @s_one || (@s_one + @s_three) <= @s_two
+      begin
+        raise TriangleError
+        puts error.message
+      end
+    elsif @s_one == @s_two && @s_one == @s_three
+      return :equilateral
+    elsif @s_one != @s_two && @s_one != @s_three && @s_two != @s_three
+      return :scalene
+    else
+      return :isosceles
+    end
   end
-  
-  def kosher_triangle 
-    x.positive? && y.positive? && z.positive?
-  end 
-  
-  def kosher_triangle2 
-    x + y > z && x + z > y && y + z > x
-  end 
+  class TriangleError < StandardError
+    def message
+      "not a triangle"
+    end
+  end
 end
-
-class TriangleError < StandardError
-  
-end
-
-a = Triangle.new(5, 5, 5)
-a.kind
-d
